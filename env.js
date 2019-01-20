c729e0f7157d25facd73677c5175c376ab2a8da0
fireofearth <colinc@protonmail.com>
2019-01-19 20:56:08 -0800

    fixed commit

--- env.js
+++
@@ -1,8 +0,0 @@
-exports.JSON_FILE = "job_list.json";
-exports.LOCK_FILE = "lock";
-exports.SPIN = 100;
-exports.mail = { };
-exports.mail.HOST = "smtp.mailtrap.io";
-exports.mail.PORT = 2525;
-exports.mail.USER = "eeac3290faceca";
-exports.mail.PASS = "f2077e357e071a";
